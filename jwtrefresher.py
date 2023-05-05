2cfaaae2f6c23d159181ea68e31cce12f577983f
tilmana <59236083+tilmana@users.noreply.github.com>
2023-05-05 17:21:31 -0400

    Update jwtrefresher.py
    Fixed SSL issues, added parameter replacement functionality, fixed JWT not being replaced in requests.

--- jwtrefresher.py
+++ jwtrefresher.py
@@ -1,26 +1,31 @@
-# planning to add separate refresh argument, fixing proxy error, and more
-
 import sys
 import requests
 import json
 import time
 import argparse
+import re
 from datetime import datetime
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 parser = argparse.ArgumentParser(description="""Refresher that helps to check absolute session timeout in applications that refresh tokens to maintain session validity""")
 parser.add_argument('-e', '--endpoint', help='Refresh endpoint', required=True)
 parser.add_argument('-j', '--jwt', type=str, help='Initial valid JWT to use', required=True)
-parser.add_argument('-m', '--method', help='HTTP method (default GET)', required=False)
+parser.add_argument('-v', '--value', type=str, help='Parameter in query string to replace with JWTs', required=False)
+parser.add_argument('-m', '--method', help='HTTP method (default POST)', required=False, default='POST', type=str)
 parser.add_argument('-p', '--proxy', help='Proxy to send requests with', required=False)
 parser.add_argument('-d', '--delay', help='Delay in seconds between requests (default 30)', required=False, default=30, type=int)
+parser.add_argument('-n', '--name', help='Name of JSON object containing new token (default \'jwt\')', required=False, type=str, default='jwt')
 parser.add_argument('-c', '--cookie', help='Set JWT into Cookie header with specified name instead of Authorization header', required=False)
 
 args = parser.parse_args()
 
-proxies = {
-    'http': f'{args.proxy}',
-    'https': f'{args.proxy}'
-}
+if args.proxy:
+    proxies = {
+        'http': f'{args.proxy}',
+        'https': f'{args.proxy}'
+    }
 
 class bcolors:
     HEADER = '\033[95m'
@@ -36,29 +41,28 @@ class bcolors:
 totalRefreshes = 0
 totalTime = 0
 
-cookieHeader = None
-requestHeader = None
 
-if args.cookie:
-    cookieHeader = {
-        args.cookie: f'{args.jwt}'
-    }
-else:
-    requestHeader = {
-        'Authorization': f'Bearer {args.jwt}'
-    }
-
-def make_request(url, method, headers=None, cookies=None, proxy=None):
+def make_request(url, method, headers=None, cookies=None, proxies=None):
     http_method = getattr(requests, method.lower())
-    res1 = http_method(url, headers=headers, cookies=cookies, proxies=proxy)
+    res1 = http_method(url, headers=headers, cookies=cookies, proxies=proxies, verify=False)
     return res1
 
-
 def refresh(jwt):
+    cookieHeader = None
+    requestHeader = None
+    if args.cookie:
+        cookieHeader = {
+            args.cookie: f'{jwt}'
+        }
+    else:
+        requestHeader = {
+            'Authorization': f'Bearer {jwt}'
+        }
     global totalRefreshes
     global totalTime
-    if not args.method:
-        r1 = make_request(args.endpoint, 'GET', requestHeader, cookieHeader, proxies)
+    if args.value:
+        pattern = r"(?<=\bPLACEHOLDER=)[^&]+".replace("PLACEHOLDER", args.value)
+        r1 = make_request(re.sub(pattern, jwt, args.endpoint), args.method, requestHeader, cookieHeader, proxies)
     else:
         r1 = make_request(args.endpoint, args.method, requestHeader, cookieHeader, proxies)
     try:
@@ -66,11 +70,11 @@ def refresh(jwt):
     except:
         print(bcolors.WARNING + f"[!] Error while parsing JSON response, exiting... {datetime.now()}" + bcolors.ENDC)
         print(bcolors.BOLD + f'[+] Total refreshes: {totalRefreshes}    Total delay time: {totalTime}')
-        sys.exit(-1)
-    if 'token' in response_data:
-        print(f"[+] New token: " + bcolors.OKCYAN + f"{response_data['token']}" + bcolors.ENDC)
+        sys.exit(0)
+    if args.name in response_data:
+        print(f"[+] New token: " + bcolors.OKCYAN + f"{response_data[args.name]}" + bcolors.ENDC)
         time.sleep(args.delay)
-        refresh(response_data['token'])
+        refresh(response_data[args.name])
         totalRefreshes += 1
         totalTime += args.delay
     else:
